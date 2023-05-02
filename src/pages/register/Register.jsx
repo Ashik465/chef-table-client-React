b5e305ff6ffd4d96bee2c3ce78837964444f73a1
Ashik <helloashikul4@gmail.com>
2023-05-02 14:25:50 +0600

    register-design-done

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,10 +1,78 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
         <div>
-            <div>this is register</div>
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col ">
+            <div className="text-center  ">
+              <h1 className="text-5xl font-bold ">Sign-Up now!</h1>
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+              <form className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    className="input input-bordered" 
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    className="input input-bordered" required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text ">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    className="input input-bordered" required
+                  />
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">
+                      Forgot password?
+                    </a>
+                  </label>
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Photo-URL</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Photo-URL"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control mt-6">
+                  <button className="btn btn-main mb-3">Sign-Up</button>
+                </div>
+  
+                
+  
+                <p>
+                  Already have an account! 
+                  <Link to="/login" className="link link-color">
+                    Login
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
