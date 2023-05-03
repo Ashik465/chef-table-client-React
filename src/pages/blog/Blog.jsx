796f60b202073752cd4e060b6cfbdac98a6c5160
Ashik <helloashikul4@gmail.com>
2023-05-03 18:35:19 +0600

    generate-pdf-in-blog-page-done

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Pdf from "react-to-pdf";
 
+const ref = React.createRef();
 const Blog = () => {
     return (
-        <div className='container mx-auto mt-20'>
+        <div ref={ref} className='container mx-auto mt-20'>
            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
   <div className="collapse-title text-xl font-medium">
   Tell us the differences between uncontrolled and controlled components.
@@ -21,13 +23,13 @@ to get form values from the DOM.</p>
   </div>
   <div className="collapse-content">
     <p> 
-    PropTypes.any: The prop can be of any data type
-PropTypes.bool: The prop should be a Boolean
-PropTypes.number: The prop should be a number
-PropTypes.string: The prop should be a string
-PropTypes.func: The prop should be a function
-PropTypes.array: The prop should be an array
-PropTypes.object: The prop should be an object
+PropTypes.any: The prop can be of any data type,
+PropTypes.bool: The prop should be a Boolean,
+PropTypes.number: The prop should be a number,
+PropTypes.string: The prop should be a string,
+PropTypes.func: The prop should be a function,
+PropTypes.array: The prop should be an array,
+PropTypes.object: The prop should be an object,
 PropTypes.symbol: The prop should be a symbol</p>
   </div>
               </div>
@@ -48,6 +50,20 @@ PropTypes.symbol: The prop should be a symbol</p>
     Custom React JS hooks offer reusability as when a custom hook is created, it can be reused easily, which makes the code cleaner and reduces the time to write the code. It also enhances the rendering speed of the code as a custom hook does not need to be rendered again and again while rendering the whole code.</p>
   </div>
               </div>
+
+  <div className='container mx-auto  my-20'>
+    
+    <h1 className='text-5xl font-bold mb-8'>Generate a pdf  of this page</h1>
+    
+    <Pdf targetRef={ref} filename="code-example.pdf">
+        {({ toPdf }) => <button className=' btn btn-main' onClick={toPdf}>Generate Pdf</button>}
+      </Pdf>
+      </div>
+     
+     
+          
+
+
         </div>
     );
 };
