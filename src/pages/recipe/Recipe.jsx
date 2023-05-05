104f8dd10071ab0f6aa9517e6d2a812764224b38
Ashik <helloashikul4@gmail.com>
2023-05-05 17:56:11 +0600

    recipe-page-updated

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -5,7 +5,7 @@ import toast, { Toaster } from 'react-hot-toast';
 
 
 const Recipe = ({ recipe }) => {
-  const { recipeName, ingredients, cookingMethod, rating } = recipe;
+  const { recipeName, ingredients, cookingMethod, rating ,recipePicture} = recipe;
  
   const[favorite,setFavorite]=useState(false)
 
@@ -19,6 +19,9 @@ const Recipe = ({ recipe }) => {
   return (
     <div>
       <div className="card w-full h-full bg-base-100 shadow-xl">
+       
+      <figure><img src={recipePicture} alt="food-picture" /></figure>
+       
         <div className="card-body">
           <h2 className="card-title font-bold text-3xl">{recipeName}</h2>
           <>
@@ -33,12 +36,15 @@ const Recipe = ({ recipe }) => {
           </>
 
           <p> <span className=" font-bold">Cooking-Method:</span>  {cookingMethod}</p>
-            
-            <Rating
+
+           <div className="flex items-center gap-4 ">
+            <div className="text-xl">{rating}</div>
+            <div><Rating
       style={{ maxWidth: 180 }}
       value={rating}
       readOnly
-    />
+    /></div></div> 
+            
             
           <div className="card-actions justify-end">
             <button onClick={handleToast} disabled={favorite}  className=" btn btn-main">Favorite <FaHeart></FaHeart></button>
